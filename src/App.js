e94808bcbdbf5628fbbdefd9652fb9137b1ae8dd
manue rodriguez <manuel.rodriguez@jdpa.com>
2022-02-15 11:55:08 -0800

    upudate menuItem to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
-import Homepage from './pages/homepage/Homepage';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/homepage/Homepage";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
