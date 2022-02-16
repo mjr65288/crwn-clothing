08897706bffbc967835a2237f4c6e16cd12022fc
manue rodriguez <manuel.rodriguez@jdpa.com>
2022-02-15 18:46:13 -0800

    adding sign-in page and signin, forminput, and custombutton components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sign-in-and-sign-up.scss";
+import SigIn from "../../components/sign-in/sign-in";
+
+const SignInAndSignUp = () => {
+  return (
+    <div>
+      <SigIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUp;
